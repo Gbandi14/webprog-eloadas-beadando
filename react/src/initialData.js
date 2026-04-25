1e689439b16340129e5a46043655fad728f2faae
juzso <tyityo123@gmail.com>
2026-04-25 13:20:53 +0200

    react src js hozzáadása

---
+++ react/src/initialData.js
@@ -0,0 +1,62 @@
+export const initialVizsgatargyak = [
+  {
+    "azon": 1,
+    "nev": "Magyar nyelv és irodalom",
+    "szomax": 50,
+    "irmax": 100
+  },
+  {
+    "azon": 2,
+    "nev": "Történelem",
+    "szomax": 60,
+    "irmax": 90
+  },
+  {
+    "azon": 3,
+    "nev": "Matematika",
+    "szomax": 35,
+    "irmax": 115
+  },
+  {
+    "azon": 4,
+    "nev": "Informatika",
+    "szomax": 30,
+    "irmax": 120
+  },
+  {
+    "azon": 5,
+    "nev": "Fizika",
+    "szomax": 50,
+    "irmax": 100
+  },
+  {
+    "azon": 6,
+    "nev": "Kémia",
+    "szomax": 50,
+    "irmax": 100
+  },
+  {
+    "azon": 7,
+    "nev": "Angol",
+    "szomax": 30,
+    "irmax": 120
+  },
+  {
+    "azon": 8,
+    "nev": "Német",
+    "szomax": 30,
+    "irmax": 120
+  },
+  {
+    "azon": 9,
+    "nev": "Földrajz",
+    "szomax": 50,
+    "irmax": 100
+  },
+  {
+    "azon": 10,
+    "nev": "Biológia",
+    "szomax": 50,
+    "irmax": 100
+  }
+];
